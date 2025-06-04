73fade23cf59535df8b7ae820055b5d066b6c1ea
Harsh Panchal <199502465+hars6@users.noreply.github.com>
2025-06-04 15:53:44 +0530

    Update read_db.py

--- face_recog_sys/read_db.py
+++ face_recog_sys/read_db.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 def display_database():
@@ -23,39 +22,3 @@ def display_database():
 
 if __name__ == "__main__":
     display_database()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import sqlite3
-#
-# # DB file ka naam yahan likh
-# db_file = 'attendance.db'
-#
-# # SQLite connection banayenge
-# conn = sqlite3.connect(db_file)
-# cursor = conn.cursor()
-#
-# # Saari tables ka naam fetch karna
-# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-# tables = cursor.fetchall()
-#
-# print("Tables in the database:")
-# for table in tables:
-#     print(f"\n--- Data from table: {table[0]} ---")
-#     cursor.execute(f"SELECT * FROM {table[0]}")
-#     rows = cursor.fetchall()
-#     for row in rows:
-#         print(row)
-#
-# # Connection close karna
-# conn.close()
